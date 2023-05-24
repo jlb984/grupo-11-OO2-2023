c659e1a88f902d2e8a2370c3b2cba99621cccb38
jlb984 <jlb984@gmail.com>
2023-05-23 22:38:17 -0300

    se genera el proyecto con Spring boot y se corre el build para generar la estructura del mismo

---
+++ src/test/java/com/grupo11/grupo11OO22023/Grupo11Oo22023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo11.grupo11OO22023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo11Oo22023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
