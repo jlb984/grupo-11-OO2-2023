422e7c20a77c81dbd0bc0102487e99892c485fb4
Lucas Martinez <43049088l.m@gmail.com>
2023-06-19 16:58:50 -0300

    Herencia

--- src/main/java/com/grupo11/grupo11OO22023/entities/DispositivoIOT.java
+++ src/main/java/com/grupo11/grupo11OO22023/entities/DispositivoIOT.java
@@ -6,6 +6,8 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.Getter;
@@ -13,6 +15,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 @Getter @Setter @NoArgsConstructor
 public abstract class DispositivoIOT {
 
@@ -40,7 +43,6 @@ public abstract class DispositivoIOT {
 		this.estaActivo = estaActivo;
 		this.descripcion = descripcion;
 	}
-	
 	
 	
 }
